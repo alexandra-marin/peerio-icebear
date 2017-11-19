d6b7e86741d5c319f5e260581f768b9027811261
Anri Asaturov <anri82@gmail.com>
2017-11-19 17:40:38 +0200

    test: configure artifacts

--- test/e2e/run-wip.sh
+++ test/e2e/run-wip.sh
@@ -1,8 +1,5 @@
-mkdir -p ./test-results/e2e
 node --expose-gc ./node_modules/.bin/cucumber.js test/e2e/spec \
         -r test/e2e/code \
         --compiler js:babel-register \
         --format node_modules/cucumber-pretty \
-        --format usage:./test-results/e2e/usage.txt \
-        --format json:./test-results/e2e/result.json \
-        --tags 'not @wip'
+        --tags '@wip'

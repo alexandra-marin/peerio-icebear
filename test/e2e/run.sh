957aac2063b4313443de1104419cd0b696171b5c
Anri Asaturov <anri82@gmail.com>
2018-01-18 04:29:14 +0200

    test: skip long tests on CI

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,8 +1,16 @@
+set -x
 mkdir -p ./test-results/e2e
+
+if [ $CI ]
+then
+    tags='not @wip and not @long'
+else
+    tags='not @wip'
+fi
 node --expose-gc ./node_modules/.bin/cucumber.js test/e2e/spec \
         -r test/e2e/code \
         --compiler js:babel-register \
         --format node_modules/cucumber-pretty \
         --format usage:./test-results/e2e/usage.txt \
         --format json:./test-results/e2e/result.json \
-        --tags 'not @wip'
+        --tags "$tags"

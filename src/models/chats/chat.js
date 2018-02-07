73935e9a1a1caca8572f07cbf2c4fd96756294af
Anri Asaturov <anri82@gmail.com>
2018-02-07 20:11:38 +0200

    chore: enable rekey

--- src/models/chats/chat.js
+++ src/models/chats/chat.js
@@ -1153,10 +1153,12 @@ class Chat {
                 () => {
                     if (wasAdmin) boot.unassignRole(contact, 'admin');
                     boot.removeParticipant(contact);
+                    boot.addKey();
                     return true;
                 },
                 () => {
                     boot.addParticipant(contact);
+                    boot.removeUnsavedKey();
                     if (wasAdmin) boot.assignRole(contact, 'admin');
                 },
                 'error_removeParticipant'

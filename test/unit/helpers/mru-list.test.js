1b3a73d1fdffbeb4c2a82bad2a2e832bc56fe8d6
Alexandra Marin <alexandra-marin@users.noreply.github.com>
2018-02-16 19:50:35 +0200

    test: MRU list (#149)
    * test: MRU list
    
    * fix: compare observable with observable

--- test/unit/helpers/mru-list.test.js
+++ test/unit/helpers/mru-list.test.js
@@ -1,21 +1,39 @@
-// const MRUList = require('../../src/helpers/mru-list');
+const MRUList = require('~/helpers/mru-list');
+const TinyDb = require('~/db/tiny-db');
+const StorageEngine = require('~/models/storage/node-json-storage');
+const config = require('~./config');
+const { observable } = require('mobx');
 
-// describe('MRU list helper should', () => {
-//     let mruList;
-//     const items = [];
+describe('MRU list helper should', () => {
+    before(() => {
+        config.StorageEngine = StorageEngine;
+        TinyDb.openUser('test', 'test');
+    });
 
-//     it('delete items when size limit is exceeded', () => {
-//         const expected = 1;
-//         const actual = 1;
+    it('order items by last added', () => {
+        const list = new MRUList('testList', 3);
+        list.addItem(1);
+        list.addItem(2);
+        list.addItem(3);
 
-//         actual.should.equal(expected);
-//     });
+        const expected = observable([3, 2, 1]);
+        const actual = list.list;
 
-//     it('return contained items in order', () => {
-//         const expected = 1;
-//         const actual = 1;
+        actual.should.deep.equal(expected);
+    });
 
-//         actual.should.equal(expected);
-//     });
-// });
+    it('delete items when size limit is exceeded', async () => {
+        const list = new MRUList('testList', 3);
+        list.addItem(1);
+        list.addItem(2);
+        list.addItem(3);
+        list.addItem(4);
+        list.addItem(5);
+
+        const expected = observable([5, 4, 3]);
+        const actual = list.list;
+
+        actual.should.deep.equal(expected);
+    });
+});
 

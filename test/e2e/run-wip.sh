94ab4159048929857e9bcde44a6176d9ce22cd5b
Anri Asaturov <anri82@gmail.com>
2018-02-21 23:44:50 +0200

    chore: trigger build

--- test/e2e/run-wip.sh
+++ test/e2e/run-wip.sh
@@ -1,5 +1,6 @@
 export SHOW_APP_LOGS=1
 export PEERIO_REDUCE_SCRYPT_FOR_TESTS=1
+
 node --expose-gc ./node_modules/.bin/cucumber-js test/e2e/spec \
         -r test/e2e/code \
         --require-module babel-register \

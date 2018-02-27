7497faeb4abbd456fb99d3ee0ed43076174238c7
Lucas <lucas@superko.org>
2018-02-27 09:58:56 -0500

    chore: remove redundant 'selected' bool

--- src/models/volumes/volume.js
+++ src/models/volumes/volume.js
@@ -1,8 +1,6 @@
 const AbstractFolder = require('../files/abstract-folder');
-const { observable } = require('mobx');
 
 class Volume extends AbstractFolder {
-    @observable selected = false;
     isShared = true;
 }
 

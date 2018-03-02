64a72ef7046cd50b58346e4486fa79585f0eef3b
Samvel Avanesov <seavan@gmail.com>
2018-03-02 01:09:42 -0500

    feat: delete files confirmation

--- src/models/files/file-store.bulk.js
+++ src/models/files/file-store.bulk.js
@@ -7,6 +7,10 @@ class FileStoreBulk {
     // functor taking folder as an argument to confirm folder deletion
     deleteFolderConfirmator = null;
 
+    // functor taking files and shared fiels as an argument to confirm
+    // folder deletion
+    deleteFolderConfirmator = null;
+
     constructor(fileStore) {
         this.fileStore = fileStore;
     }
@@ -29,6 +33,11 @@ class FileStoreBulk {
 
     @action.bound async remove() {
         const items = this.fileStore.selectedFilesOrFolders;
+        if (this.deleteFilesConfirmator) {
+            const files = items.filter(i => !i.isFolder);
+            const sharedFiles = items.some(i => i.shared);
+            if (files.length && !await this.deleteFilesConfirmator(files, sharedFiles)) return;
+        }
         let promise = Promise.resolve();
         items.forEach(i => {
             promise = promise.then(() => this.removeOne(i, true));

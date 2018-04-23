bd6c9688498b0c1258e1f9481fc7780806d19c0f
Samvel Avanesov <seavan@gmail.com>
2018-04-23 16:25:07 +0200

    chore: temporary disable applabel

--- src/models/user/user.auth.js
+++ src/models/user/user.auth.js
@@ -78,7 +78,7 @@ module.exports = function mixUserAuthModule() {
                 arch: config.arch,
                 clientVersion: config.appVersion,
                 sdkVersion: config.sdkVersion,
-                appLabel: config.appLabel,
+                // appLabel: config.appLabel,
                 // sending whatever string in the beginning to let server know we are
                 // a new, cool client which is gonna use sessions
                 sessionId: this.sessionId || 'initialize'

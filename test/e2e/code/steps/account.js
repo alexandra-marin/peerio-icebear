1095fe903b69ca91bd1c77f6b5c49f418a865358
Alexandra Marin <alexandra.marin01@gmail.com>
2018-05-10 13:37:28 +0100

    WIP: move to own file

--- test/e2e/code/steps/account.js
+++ test/e2e/code/steps/account.js
@@ -122,35 +122,3 @@ Then('I have received all bonuses', async function() {
     await this.waitFor(() => this.ice.User.current.hasInstallBonus);
 });
 
-Given('I create a MedCryptor account', { timeout: 60000 }, async function() {
-    this.ice.config.appLabel = 'medcryptor';
-    const medcryptorData = {
-        specialization: 'cardiology',
-        medicalID: '001',
-        country: 'Canada',
-        role: 'doctor'
-    };
-
-    await this.createMedcryptorAccount(medcryptorData);
-    await this.app.restart();
-    await this.login();
-
-    this.ice.User.current.props.should.deep.equal(medcryptorData);
-});
-
-Then('I can edit specialization, medical ID, country and role', async function() {
-    const medcryptorData = {
-        specialization: 'admin',
-        medicalID: '002',
-        country: 'Australia',
-        role: 'admin'
-    };
-
-    this.ice.User.current.props = medcryptorData;
-    await this.ice.User.current.saveProfile();
-
-    await this.app.restart();
-    await this.login();
-
-    this.ice.User.current.props.should.deep.equal(medcryptorData);
-});
--- test/e2e/spec/account/medcryptor.feature
+++ test/e2e/spec/account/medcryptor.feature
@@ -1,6 +1,5 @@
 Feature: MedCryptor users
 
-    @wip
     Scenario: MedCryptor users have extra data
         Given I create a MedCryptor account
         Then  I can edit specialization, medical ID, country and role

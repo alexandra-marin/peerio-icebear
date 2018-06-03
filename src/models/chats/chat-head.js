8bd36048353f6fdf2a9e03f5dc92dd4bb9ab0684
Alexandra Marin <alexandra.marin01@gmail.com>
2018-06-03 16:35:47 +0300

    fix: copy and nameInSpace

--- src/models/chats/chat-head.js
+++ src/models/chats/chat-head.js
@@ -46,6 +46,14 @@ class ChatHead extends SyncedKeg {
     @observable spaceName = null;
 
     /**
+     * @member {string} nameInSpace
+     * @memberof ChatHead
+     * @instance
+     * @public
+     */
+    @observable nameInSpace = null;
+
+    /**
      * @member {string} spaceDescription
      * @memberof ChatHead
      * @instance
@@ -68,6 +76,7 @@ class ChatHead extends SyncedKeg {
             purpose: this.purpose,
             spaceId: this.spaceId,
             spaceName: this.spaceName,
+            nameInSpace: this.nameInSpace,
             spaceDescription: this.spaceDescription,
             spaceRoomType: this.spaceRoomType
         };
@@ -78,6 +87,7 @@ class ChatHead extends SyncedKeg {
         this.purpose = payload.purpose || '';
         this.spaceId = payload.spaceId;
         this.spaceName = payload.spaceName;
+        this.nameInSpace = payload.nameInSpace;
         this.spaceDescription = payload.spaceDescription;
         this.spaceRoomType = payload.spaceRoomType;
     }

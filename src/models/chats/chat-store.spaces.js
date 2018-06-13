105a3321bf2cfd34e43ccf2a2aa247c9c1db100d
Alexandra Marin <alexandra.marin01@gmail.com>
2018-06-13 13:12:00 +0300

    WIP: create room in patient space

--- src/models/chats/chat-store.spaces.js
+++ src/models/chats/chat-store.spaces.js
@@ -71,6 +71,34 @@ class ChatStoreSpaces {
 
         return space;
     }
+
+    createNewInternalRoom = () => { this.createRoomInPatientSpace('internal'); }
+    createNewPatientRoom = () => { this.createRoomInPatientSpace('patient'); }
+
+    /**
+     * @returns {Chat}
+     */
+    createRoomInPatientSpace = async (SPACE, type) => {
+        const roomSpaceProperties = {
+            spaceId: SPACE.currentSpace.spaceId,
+            spaceName: SPACE.currentSpace.spaceName,
+            nameInSpace: this.channelName,
+            spaceDescription: SPACE.currentSpace.spaceDescription,
+            spaceRoomType: type
+        };
+
+        const name = `${SPACE.currentSpace.spaceName} - ${this.channelName}`;
+        const chat = await this.startChat(this.userPicker.selected, true, name, '', true, roomSpaceProperties);
+
+        return chat;
+        // if (!chat) {
+        //     this.waiting = false;
+        //     return;
+        // }
+        // when(() => chat.added === true, () => {
+        //     routerStore.navigateTo(routerStore.ROUTES.patients);
+        // });
+    }
 }
 
 module.exports = ChatStoreSpaces;

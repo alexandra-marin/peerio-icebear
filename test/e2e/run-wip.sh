74a29258430ec7037d2ee48c8b1134199285a481
Anri Asaturov <anri82@gmail.com>
2018-07-26 20:41:53 +0300

    chore: change build step configuration to run on ci

--- test/e2e/run-wip.sh
+++ test/e2e/run-wip.sh
@@ -3,6 +3,8 @@
 export SHOW_APP_LOGS=1
 export PEERIO_REDUCE_SCRYPT_FOR_TESTS=1
 
+npm run test-build
+
 node --expose-gc ./node_modules/.bin/cucumber-js test/e2e/spec \
         -r test/e2e/code \
         --format node_modules/cucumber-pretty \

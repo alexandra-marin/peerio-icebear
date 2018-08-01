99d430cb46390fcd65527e74f470ac9081d26e9d
suprko <lucas@peerio.com>
2018-08-01 12:53:57 -0400

    chore: move error_messages to const outside function

--- src/telemetry/helpers.js
+++ src/telemetry/helpers.js
@@ -1,24 +1,22 @@
 const S = require('./strings');
 
+const ERROR_MESSAGES = {
+    error_usernameBadFormat: 'Bad Characters',
+    error_usernameNotFound: 'Username Not Found',
+    error_usernameNotAvailable: 'Name Already Exists',
+    error_invalidEmail: 'Not Valid Email',
+    error_addressNotAvailable: 'Email Not Available',
+    error_fieldRequired: 'Required',
+    error_loginFailed: 'Sign-in Failed'
+};
+
 function errorMessage(msg) {
-    const messageNames = {
-        error_usernameBadFormat: 'Bad Characters',
-        error_usernameNotFound: 'Username Not Found',
-        error_usernameNotAvailable: 'Name Already Exists',
-        error_invalidEmail: 'Not Valid Email',
-        error_addressNotAvailable: 'Email Not Available',
-        error_fieldRequired: 'Required',
-        error_loginFailed: 'Sign-in Failed'
-    };
-    return messageNames[msg] || msg || 'Unknown Error Type';
+    return ERROR_MESSAGES[msg] || msg || 'Unknown Error Type';
 }
 
 // Factory for text input events
 function textInput(item, location, sublocation, state, errorMsg) {
-    const ret = [
-        S.TEXT_INPUT,
-        { item, state }
-    ];
+    const ret = [S.TEXT_INPUT, { item, state }];
 
     if (location) ret[1].location = location;
     if (sublocation) ret[1].sublocation = sublocation;
@@ -31,10 +29,7 @@ function textInput(item, location, sublocation, state, errorMsg) {
 function duration(item, location, sublocation, startTime) {
     const totalTime = Math.round((Date.now() - startTime) / 1000);
 
-    const ret = [
-        S.DURATION,
-        { totalTime }
-    ];
+    const ret = [S.DURATION, { totalTime }];
 
     if (item) ret[1].item = item;
     if (location) ret[1].location = location;

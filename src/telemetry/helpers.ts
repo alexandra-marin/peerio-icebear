20f244c4b9d077f7ed88207cf2fcbe95bdb67080
suprko <lucas@peerio.com>
2018-08-30 12:18:11 -0400

    chore: remove duration and textInput telemetry factory functions

--- src/telemetry/helpers.ts
+++ src/telemetry/helpers.ts
@@ -1,5 +1,3 @@
-import S from './strings';
-
 const ERROR_MESSAGES = {
     error_usernameBadFormat: 'Bad Characters',
     error_usernameNotFound: 'Username Not Found',
@@ -12,49 +10,6 @@ const ERROR_MESSAGES = {
     error_wrongAK: 'Invalid Account Key'
 };
 
-function errorMessage(msg: string): string {
+export function errorMessage(msg: string): string {
     return ERROR_MESSAGES[msg] || msg || 'Unknown Error Type';
-}
-
-// Factory for text input events
-interface TextInputProps {
-    item: string;
-    location?: string;
-    sublocation?: string;
-    state: string;
-    errorType?: string;
-}
-export function textInput(
-    item: string,
-    location?: string,
-    sublocation?: string,
-    state?: string,
-    errorMsg?: string
-) {
-    const properties: TextInputProps = { item, state };
-
-    if (location) properties.location = location;
-    if (sublocation) properties.sublocation = sublocation;
-    if (errorMsg) properties.errorType = errorMessage(errorMsg);
-
-    return [S.TEXT_INPUT, properties];
-}
-
-// Factory for duration events
-interface DurationProps {
-    item?: string;
-    location?: string;
-    sublocation?: string;
-    totalTime: number;
-}
-export function duration(item: string, location: string, sublocation: string, startTime: number) {
-    const totalTime = Math.round((Date.now() - startTime) / 1000);
-
-    const properties: DurationProps = { totalTime };
-
-    if (item) properties.item = item;
-    if (location) properties.location = location;
-    if (sublocation) properties.sublocation = sublocation;
-
-    return [S.DURATION, properties];
 }

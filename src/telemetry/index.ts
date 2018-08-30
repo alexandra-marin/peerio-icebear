aa14312d279e6d054e51f0ac03a5d0dcea4bc454
suprko <lucas@peerio.com>
2018-08-30 14:22:33 -0400

    chore: move types from desktop

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -1,13 +1,15 @@
 import S from './strings';
 import { init, send } from './main';
 import { duration, errorMessage } from './helpers';
+import * as types from './types';
 
 const telemetry = {
     S,
     init,
     send,
     duration,
-    errorMessage
+    errorMessage,
+    types
 };
 
 export default telemetry;

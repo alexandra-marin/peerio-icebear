fe9a92aee5050859424bbeabad65f647d8e8416a
suprko <lucas@peerio.com>
2018-09-05 15:11:51 -0400

    chore: comment

--- src/telemetry/main.ts
+++ src/telemetry/main.ts
@@ -1,4 +1,3 @@
-// Main telemetry functionality is here, most importantly the send() export
 const config = require('../config');
 const TinyDb = require('../db/tiny-db');
 const { cryptoUtil } = require('../crypto');

4665899ab13d421c3f0a2e9cd0abf6ba9adf9fd7
suprko <lucas@peerio.com>
2018-09-20 10:10:33 -0400

    chore: spacing

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -1,5 +1,4 @@
 export { default as S } from './strings';
 export { send, bulkSend } from './main';
 export { duration, errorMessage } from './helpers';
-
 export { EventObject, EventProperties } from './types';

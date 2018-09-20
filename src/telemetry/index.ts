941a85192ab2519c61fad4b2fdcf36aca29ddd88
suprko <lucas@peerio.com>
2018-09-20 16:14:56 -0400

    fix: sendStored export

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -1,5 +1,5 @@
 export { default as S } from './strings';
-export { send } from './main';
+export { send, sendStored } from './main';
 export { duration, errorMessage } from './helpers';
 export { default as store } from './store';
 export { EventObject, EventProperties } from './types';

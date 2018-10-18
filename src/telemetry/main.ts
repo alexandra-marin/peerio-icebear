28ead71c43776f98b8f26e1480cb3409808e7da1
suprko <lucas@peerio.com>
2018-10-18 11:56:33 -0400

    WIP: add eventTime prop to telemetry events [branch ch15434]

--- src/telemetry/main.ts
+++ src/telemetry/main.ts
@@ -80,18 +80,21 @@ function convertEventPropertyCase(event: EventObject): EventProperties {
 let eventStore: EventObject[] = [];
 
 export async function send(eventObj: EventObject): Promise<void> {
+    const eventWithTime = eventObj;
+    eventWithTime.properties.eventTime = Date.now();
+
     if (!User.current || !User.current.settings.loaded) {
-        eventStore.push(eventObj);
+        eventStore.push(eventWithTime);
         return;
     }
     if (!User.current.settings.dataCollection) return;
 
     try {
         const baseProperties = await getBaseProperties();
-        const eventProperties = convertEventPropertyCase(eventObj);
-        eventObj.properties = { ...baseProperties, ...eventProperties };
+        const eventProperties = convertEventPropertyCase(eventWithTime);
+        eventWithTime.properties = { ...baseProperties, ...eventProperties };
 
-        const data = bytesToB64(strToBytes(JSON.stringify(eventObj)));
+        const data = bytesToB64(strToBytes(JSON.stringify(eventWithTime)));
         const url = `${config.telemetry.baseUrl}?data=${data}`;
 
         await fetch(url, {

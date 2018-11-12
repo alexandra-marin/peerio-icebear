5c75f422a7be8f2731c57cc7df31083cd5a88463
Dmitry Chestnykh <dmitry@codingrobots.com>
2018-11-12 16:52:14 +0100

    test: use memory storage for mru test

---
+++ src/models/storage/memory-storage.ts
@@ -0,0 +1,31 @@
+import { TinyDBStorageEngine } from '../../defs/tiny-db';
+
+export default class MemoryStorage implements TinyDBStorageEngine {
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    name: string;
+    private data: { [key: string]: string } = {};
+
+    // should return null if value doesn't exist
+    async getValue(key) {
+        return this.data.hasOwnProperty(key) ? this.data[key] : null;
+    }
+
+    async setValue(key, value) {
+        this.data[key] = value;
+    }
+
+    async removeValue(key) {
+        delete this.data[key];
+    }
+
+    async getAllKeys() {
+        return Object.keys(this.data);
+    }
+
+    async clear() {
+        this.data = {};
+    }
+}
